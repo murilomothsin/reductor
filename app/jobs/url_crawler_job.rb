2c7faeab1832ba5adff1f1acf85f2607201a16b2
Murilo Mothsin <mothsin@hotmail.com>
2019-04-29 21:17:10 -0300

    Fixbug on Crawler job

--- app/jobs/url_crawler_job.rb
+++ app/jobs/url_crawler_job.rb
@@ -5,7 +5,7 @@ class UrlCrawlerJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    Short.all do |short|
+    Short.all.each do |short|
       doc = Nokogiri::HTML(open(short.url))
       short.update_attribute(:page_title, doc.at_css('title').text)
     end
